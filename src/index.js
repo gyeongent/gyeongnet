40548ee83ba9138ffdddd7937ed849dfcc06809f
IMJGyeongMin <59150207+gyeongent@users.noreply.github.com>
2022-07-29 13:49:25 +0900

    Scroll Top patch

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom'
 import './index.css';
+import ScrollTop from './componenets/ScrollTop';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
@@ -9,6 +10,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
+        <ScrollTop />
         <App />
       </BrowserRouter>
   </React.StrictMode>
