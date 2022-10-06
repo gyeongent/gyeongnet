024cb6f582d584c47ee72738bd7c193a65884399
GyeongMin Lee <59150207+gyeongent@users.noreply.github.com>
2022-10-06 16:08:32 +0900

    funny feature

--- src/componenets/download.js
+++ src/componenets/download.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import SoonImg from '../img/soon.jpg';
 import GameCtrlIcon from '../img/game_controller.png';
 import Launchpad from '../img/launchpad.png';
 import Font from '../img/font.png';
@@ -68,6 +69,17 @@ function DetailContent(props){
 }
 
 function DetailSection(){
+
+    document.addEventListener('scroll', function(){
+        var st = document.getElementsByClassName('SideTitle')[0]
+
+        if(window.scrollY > 700){
+            st.classList.add('fixed');
+        } else {
+            st.classList.remove('fixed');
+        }
+    })
+
     return(
         <div className="DSBox">
             <div>
@@ -77,6 +89,28 @@ function DetailSection(){
                 <DetailContent DtImg={HHBD} DtTitle="해피! 혀누의 생일" DtDesc={"2022년 6월 13일 출시 예정\n혀누의 생일을 기념해 만든 게임, 각종 미니게임들이 수록되어있다."}/>
             </div>
             <hr className="DetailHr"/>
+            <div>
+                <div className='DSBTitle'>
+                    폰트
+                </div>
+                <DetailContent DtImg={SoonImg} DtTitle="안반듯체" DtDesc={"개발 일시 중단\n반듯하면서 반듯하지 않은 안반듯체."}/>
+            </div>
+            <hr className="DetailHr"/>
+            <div>
+                <div className='DSBTitle'>
+                    런치패드 프로젝트
+                </div>
+                <DetailContent DtImg={SoonImg} DtTitle="OMFG - Everybody" DtDesc={"2017년 7월 16일 업로드 | 난이도 : ★★☆☆☆\n초보자용 프로젝트."}/>
+                <DetailContent DtImg={SoonImg} DtTitle="TheFatRat - Monody 1인용 리메이크" DtDesc={"2017년 12월 3일 업로드 | 난이도 : ★★★☆☆\nNev의 2인 프로젝트 'Monody'를 1인 프로젝트로 각색한 버전."}/>
+                <DetailContent DtImg={SoonImg} DtTitle="OMFG - Ice Cream 리메이크" DtDesc={"2017년 12월 23일 업로드 | 난이도 : ★★★☆☆\n공대생 변승주의 오리지널 런치패드 프로젝트를 리메이크한 프로젝트."}/>
+                <DetailContent DtImg={SoonImg} DtTitle="워너원 - Beautiful" DtDesc={"2017년 12월 24일 업로드 | 난이도 : ★★☆☆☆\n2017년 크리스마스 기념 프로젝트"}/>
+                <DetailContent DtImg={SoonImg} DtTitle="우원재 - 시차 리메이크" DtDesc={"2018년 1월 7일 업로드 | 배포하지 않음\nLINE SWITCH 콜라보 프로젝트"}/>
+                <DetailContent DtImg={SoonImg} DtTitle="DEAMN - Rendezvous" DtDesc={"2018년 1월 30일 업로드 | 난이도 : ★★★☆☆\n"}/>
+                <DetailContent DtImg={SoonImg} DtTitle="아이유 - 분홍신" DtDesc={"2018년 12월 26일 업로드 | 난이도 : ★★★☆☆\n2018년 크리스마스 기념 프로젝트"}/>
+            </div>
+            <div className="SideTitle">
+                ARCHIVE
+            </div>
         </div>
     );
 }
