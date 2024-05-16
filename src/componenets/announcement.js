4d9f82306d3d491ed1099fc2ee0f55cac8ef4e3a
GyeongMin Lee <59150207+gyeongent@users.noreply.github.com>
2024-05-16 16:40:11 +0900

    announcement.js 업데이트

--- src/componenets/announcement.js
+++ src/componenets/announcement.js
@@ -16,7 +16,8 @@ function Announcement() {
     return (
         <div className='announcement'>
             <div className='desc'>
-                2025년, GYEONG Studio가 새롭게 출발합니다. <a href='https://blog.gyeong.net/2024/04/2024-2025-gyeong-studio.html?m=0' target='_blank' alt='GYEONG Studio Blog'><u>자세한 내용은 이쪽에서</u></a>
+                2025년, GYEONG Studio가 새롭게 출발합니다.<br/>
+                <a href='https://blog.gyeong.net/2024/04/2024-2025-gyeong-studio.html?m=0' target='_blank' alt='GYEONG Studio Blog'><u><small>자세한 내용은 이쪽에서</small></u></a>
             </div>
             <div className='desc' id='close' onClick={ closeEvent }>
                 <div className='button'>
